eed4717c9b68b34e1429f5b2a910f7ccb995e481
Pratik-Sanghavi <sanghavipratikr@gmail.com>
2024-07-15 08:13:13 -0700

    Moved browser restrictions to the super protected component

--- src/components/route_protection/SuperProtected.tsx
+++ src/components/route_protection/SuperProtected.tsx
@@ -1,14 +1,16 @@
-import React, { ReactNode, useEffect } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import { auth } from '../../firebase/firebase';
 import { signOut } from 'firebase/auth';
 import { checkSingleTab } from './sessionHandler';
 import Protected from './Protected';
+import UnsupportedBrowserPage from '../../pages/UnsupportedBrowserPage';
 
 interface SuperProtectedProps {
     children: ReactNode;
 }
 
 const SuperProtected: React.FC<SuperProtectedProps> = ({ children }) => {
+    const [isChromiumBrowser, setIsChromiumBrowser] = useState(true);
 
     const handleTabClose = async () => {
         const user = auth.currentUser;
@@ -24,6 +26,22 @@ const SuperProtected: React.FC<SuperProtectedProps> = ({ children }) => {
         window.removeEventListener('beforeunload', handleTabClose);
         };
     }, []);
+
+    useEffect(() => {
+        const isChromiumBased = () => {
+          const userAgent = navigator.userAgent;
+          const isChrome = userAgent.includes('Chrome');
+          const isEdge = userAgent.includes('Edg');
+          const isArc = userAgent.includes('Arc');
+          return isChrome || isEdge || isArc;
+        };
+        if(!isChromiumBased()) {
+          setIsChromiumBrowser(false);
+        }
+      }, []);
+    if(!isChromiumBrowser){
+        return <UnsupportedBrowserPage />;
+    }
 
     return <Protected children={children} />;
 };
