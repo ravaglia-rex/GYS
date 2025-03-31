4342e88acdfa5dcdf3b2e6a01bada7041386b6e7
Pratik-Sanghavi <sanghavipratikr@gmail.com>
2025-03-31 15:12:35 -0500

    auth to razorpay calls

--- src/db/phase1UIDCollection.ts
+++ src/db/phase1UIDCollection.ts
@@ -1,17 +1,12 @@
 import axios from "axios";
 import { PHASE_1_QUERIES_APIS, CHECK_EXAM_ID_USED } from "../constants/constants";
-import authTokenHandler from "../functions/auth_token/auth_token_handler";
 
 export const checkExamIDExists = async (examID: string): Promise<boolean> => {
     try {
-        const authToken = await authTokenHandler.getAuthToken();
         const encodedExamID = encodeURIComponent(examID);
         const config = {
             method: 'get',
-            url: `${process.env.REACT_APP_GOOGLE_CLOUD_FUNCTIONS}${PHASE_1_QUERIES_APIS}${CHECK_EXAM_ID_USED}/${encodedExamID}`,
-            headers: {
-                'Authorization': `Bearer ${authToken}`
-            }
+            url: `${process.env.REACT_APP_GOOGLE_CLOUD_FUNCTIONS}${PHASE_1_QUERIES_APIS}${CHECK_EXAM_ID_USED}/${encodedExamID}`
         };
         const response = await axios.request(config);
         const data = response.data;
